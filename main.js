7daf1991e12ae06bb7fb133b85edd7583a339b11
MattiaDG1995 <mattiadigennaro95@gmail.com>
2022-10-28 16:20:03 +0200

    fine esercizio standard

--- main.js
+++ main.js
@@ -2,28 +2,28 @@ const griglia = document.getElementById('griglia')
 console.log(griglia)
 
 function avvia(){
+   
+    griglia.innerHTML = '';
 
     function functQuadrato(){
         const div = document.createElement('div')
         div.classList.add('quadrato')
         return div
-    }
-    
-    console.log( functQuadrato() )
-    
+    }  
+    // console.log( functQuadrato() )   
     for( let i=0; i<100; i++){
-    
+        
         let elemento = functQuadrato()
         elemento.innerText= i + 1
-        console.log(elemento) 
+        // console.log(elemento) 
     
         elemento.addEventListener('click', function(){
             console.log(this)
             this.classList.toggle('attiva')
+            
         })
-    
+        
         griglia.append( elemento );
     }
-    
+   
 }
-
